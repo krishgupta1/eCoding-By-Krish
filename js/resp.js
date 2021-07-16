84e4bb7069c1273e2006007b41c675f1f0c6afe2
Krish11110 <87308081+Krish11110@users.noreply.github.com>
2021-07-16 12:33:33 +0530

    This is my eCoding Website

---
+++ js/resp.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightNav = document.querySelector('.rightNav')
+
+
+burger.addEventListener('click',()=>{
+    navList.classList.toggle('v-class-resp')
+    rightNav.classList.toggle('v-class-resp')
+    navbar.classList.toggle('h-nav-resp')
+})
